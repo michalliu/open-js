979fe2553a7a621186615dd2d43488a4763e868f
michalliu <michalliu@foxmail.com>
2012-09-04 21:24:44 +0800

    用uglifyjs转换非ascii字符到ascii,替换掉自己实现的python脚本转换

--- build.py
+++ build.py
@@ -330,7 +330,7 @@ def build(modulenames, targetfile, logFile=False):
     # pack javascript through uglifyjs
     nodejs = os.path.join(_CWD, "tools", "node.exe")
     uglifyjs = os.path.join(_CWD, "tools", "UglifyJS", "bin" ,"uglifyjs")
-    uglifyOptions = "-nc -v -o %s" % minifiedfile
+    uglifyOptions = "--ascii -nc -v -o %s" % minifiedfile
 
     uglified = os.system("%s %s %s %s" % (nodejs, uglifyjs, uglifyOptions,targetfile))
 
@@ -341,16 +341,16 @@ def build(modulenames, targetfile, logFile=False):
         packer = JavaScriptPacker()
         open(minifiedfile,"w+").write(packer.pack(finalSource))
 
-    log.info("utf-8 to ascii")
-    minifiedUtf8 = open(minifiedfile,"r").read();
-    # remove bom
-    minifiedUtf8 = minifiedUtf8.decode('utf-8-sig')
-    # non-ascii to ascii repsentation
-    minifiedUtf8 = re.sub(ur'[^\u0000-\u0127]',lambda matched : matched.group().encode('unicode_escape'),minifiedUtf8)
-    # encode to ascii
-    minifiedAscii = minifiedUtf8.encode('ascii')
-    # save ascii
-    open(minifiedfile,"w+").write(minifiedAscii)
+    #log.info("utf-8 to ascii")
+    #minifiedUtf8 = open(minifiedfile,"r").read();
+    ## remove bom
+    #minifiedUtf8 = minifiedUtf8.decode('utf-8-sig')
+    ## non-ascii to ascii repsentation
+    #minifiedUtf8 = re.sub(ur'[^\u0000-\u0127]',lambda matched : matched.group().encode('unicode_escape'),minifiedUtf8)
+    ## encode to ascii
+    #minifiedAscii = minifiedUtf8.encode('ascii')
+    ## save ascii
+    #open(minifiedfile,"w+").write(minifiedAscii)
 
 ##
 ##
