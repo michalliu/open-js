e65708c11f7355c28342b167fec897121c23048b
michalliu <michalliu@foxmail.com>
2012-09-06 19:35:08 +0800

    继续完善挂件机制

--- src/ui/widget.js
+++ src/ui/widget.js
@@ -10,6 +10,7 @@
  *           core.dom
  *           core.browser
  *           common.Array
+ *           util.deferred
  */
 (function () {
 
@@ -28,7 +29,8 @@
         padding: 10,
         defaultWidth: 200,
         defaultHeight: 200,
-        startZIndex: 1000,
+        startzIndex: 1000,
+		bottomzIndex: 900,
         margin: {
 			top:60,right:10,bottom:10,left:10
 		}
@@ -36,15 +38,15 @@
 
 	function WidgetWindow(width, height) {
 		var wrapperContainer, contentContainer, closeBtn, that=this;
-		this.width = width || widgetWindowConfig.defaultWidth;
-		this.height = height || widgetWindowConfig.defaultHeight;
-		this.wid = 'openjs_widget_' + _.uid(5);
-		this.wzIndex = widgetWindowConfig.startZIndex ++;
-		this.containerIsVisible = false;
+		this._width = width || widgetWindowConfig.defaultWidth;
+		this._height = height || widgetWindowConfig.defaultHeight;
+		this._wid = 'openjs_widget_' + _.uid(5);
+		this._wzIndex = widgetWindowConfig.startzIndex ++;
+		this._containerIsVisible = false;
 
-		this.widgetContainer = _d.createElement('div', {
-			id: this.wid,
-			style: 'position:absolute;padding:'+ widgetWindowConfig.padding +'px;overflow:hidden;z-index:' + this.wzIndex + ';'+ ((_br.msie && _br.version < 9) ? 'filter: progid:DXImageTransform.Microsoft.Gradient(GradientType=0, StartColorStr="#4c000000", EndColorStr="#4c000000");' : 'background-color:rgba(0,0,0,.3);') + 'border-radius:' + widgetWindowConfig.borderRadius + 'px;'
+		this._widgetContainer = _d.createElement('div', {
+			id: this._wid,
+			style: 'position:absolute;padding:'+ widgetWindowConfig.padding +'px;overflow:hidden;z-index:' + this._wzIndex + ';'+ ((_br.msie && _br.version < 9) ? 'filter: progid:DXImageTransform.Microsoft.Gradient(GradientType=0, StartColorStr="#4c000000", EndColorStr="#4c000000");' : 'background-color:rgba(0,0,0,.3);') + 'border-radius:' + widgetWindowConfig.borderRadius + 'px;'
 		});
 
 		wrapperContainer = _d.createElement('div', {
@@ -71,46 +73,46 @@
 
 		wrapperContainer.appendChild(closeBtn);
 		wrapperContainer.appendChild(contentContainer);
-		this.widgetContainer.appendChild(wrapperContainer);
+		this._widgetContainer.appendChild(wrapperContainer);
 	}
 
 	WidgetWindow.prototype = {
 		setWidth: function (width) {
-			this.width = width;
-			this.reflow();
-			this.repaint();
+			this._width = width;
+			this._reflow();
+			this._repaint();
 		},
 		setContentWidth: function (width) {
-			this.width = width + widgetWindowConfig.margin.left + widgetWindowConfig.margin.right;
-			this.reflow();
-			this.repaint();
+			this._width = width + widgetWindowConfig.margin.left + widgetWindowConfig.margin.right;
+			this._reflow();
+			this._repaint();
 		},
 		setHeight: function (height) {
-			this.height = height;
-			this.reflow();
-			this.repaint();
+			this._height = height;
+			this._reflow();
+			this._repaint();
 		},
 		setContentHeight: function (height) {
-			this.height = height + widgetWindowConfig.margin.top + widgetWindowConfig.margin.bottom;
-			this.reflow();
-			this.repaint();
+			this._height = height + widgetWindowConfig.margin.top + widgetWindowConfig.margin.bottom;
+			this._reflow();
+			this._repaint();
 		},
 		setDimension: function (width, height) {
-			this.width = width;
-			this.height = height;
-			this.reflow();
-			this.repaint();
+			this._width = width;
+			this._height = height;
+			this._reflow();
+			this._repaint();
 		},
 		setContentDimension: function (width, height) {
 			var w = width + widgetWindowConfig.margin.left + widgetWindowConfig.margin.right;
 			var h = height + widgetWindowConfig.margin.top + widgetWindowConfig.margin.bottom;
 			this.setDimension(w,h);
 		},
-		reflow: function () {
-			var widgetContainer = this.widgetContainer;
+		_reflow: function () {
+			var widgetContainer = this._widgetContainer;
 			var contentContainer = this.getContainer();
-			var w = this.width;
-			var h = this.height;
+			var w = this._width;
+			var h = this._height;
             var offsettop = document.documentElement.scrollTop || document.body.scrollTop;
 			contentContainer.style.width = w -  widgetWindowConfig.margin.left - widgetWindowConfig.margin.right + 'px';
 			contentContainer.style.height = (h - widgetWindowConfig.margin.top - widgetWindowConfig.margin.bottom) + 'px'; 
@@ -121,64 +123,76 @@
             widgetContainer.style.left = Math.max(0,(_br.viewport.width - (w + widgetWindowConfig.padding * 2))) / 2 + 'px';
             widgetContainer.style.top = offsettop + Math.max(0,(_br.viewport.height - (h + widgetWindowConfig.padding * 2))) / 2 + 'px';
 		},
-		repaint: function () {
+		_repaint: function () {
 			var container = this.getContainer();
 			container.style.visibility = 'hidden';
 			container.style.visibility = 'visible';
+			this._widgetContainer.style.zIndex=this._wzIndex + '';
 		},
 		show: function () {
-			var container = this.widgetContainer;
-			this.reflow();
+			var container = this._widgetContainer;
+			this._reflow();
 			container.style.display = "block";
-			this.repaint();
-			if (_.find('#' + this.wid).length < 1) {
+			this._repaint();
+			if (_.find('#' + this._wid).length < 1) {
 				document.body.appendChild(container);
 			}
-			this.containerIsVisible = true;
+			this._containerIsVisible = true;
 		},
 		hide: function () {
-			var container = this.widgetContainer;
+			var container = this._widgetContainer;
 			container.style.display = "none";
-			this.containerIsVisible = false;
+			this._containerIsVisible = false;
 		},
-		remove: function () {
-			var container = this.widgetContainer;
+		_remove: function () {
+			var container = this._widgetContainer;
 			if (container.parentNode) {
                 container.parentNode.removeChild(container);
 			}
-			this.containerIsVisible = false;
+			this._containerIsVisible = false;
 		},
 		isAlive: function () {
-            return T.find('#' + this.wid).length > 0;
+            return _.find('#' + this._wid).length > 0;
 		},
 		isVisible: function () {
-			return this.isAlive() && this.containerIsVisible;
+			return this.isAlive() && this._containerIsVisible;
 		},
 		getContainer: function () {
-			return this.widgetContainer.firstChild.childNodes[3];
-		},
-		getContentDimension: function () {
-			return {
-				width: this.width -  widgetWindowConfig.margin.left - widgetWindowConfig.margin.right,
-				height: this.height - widgetWindowConfig.margin.top - widgetWindowConfig.margin.bottom 
-			};
+			return this._widgetContainer.firstChild.childNodes[3];
 		},
 		getDimension: function () {
 			return {
-				width: this.width,
-				height: this.height,
-				outerWidth: this.width +  widgetWindowConfig.padding * 2,
-				outerHeight: this.height +  widgetWindowConfig.padding * 2
+				width: this._width,
+				height: this._height,
+				outerWidth: this._width +  widgetWindowConfig.padding * 2,
+				outerHeight: this._height +  widgetWindowConfig.padding * 2
+			};
+		},
+		getContentDimension: function () {
+			return {
+				width: this._width -  widgetWindowConfig.margin.left - widgetWindowConfig.margin.right,
+				height: this._height - widgetWindowConfig.margin.top - widgetWindowConfig.margin.bottom 
 			};
 		},
 		close: function () {
-			this.remove();
+			this._remove();
 		},
 		onCloseButtonClicked: function (handler) {
 			this.closeHandler = handler;
 		},
+		_setzIndex: function (index) {
+			this._lastwzIndex = this._wzIndex;
+			this._wzIndex = index;
+			if (this.isAlive()) this._repaint();
+		},
 		setTopMost: function () {
-			if (this.isAlive()) this.widgetContainer.style.zIndex=++widgetWindowConfig.startZIndex;
+			this._setzIndex(++widgetWindowConfig.startzIndex);
+		},
+		setBottomMost: function () {
+			this._setzIndex(widgetWindowConfig.bottomzIndex);
+		},
+		restorezIndex: function () {
+			this._setzIndex(this._lastwzIndex);
 		}
 	};
 
@@ -205,25 +219,36 @@
 				var loginstatus = _.loginStatus();
 				var loginid = 'openjs_widget_login_' + _.uid(5);
 				var logoutid = 'openjs_widget_logout_' + _.uid(5);
-				var widgetWindow;
+				var widgetWindow, con;
 				if (!loginstatus) {
-					widgetWindow = new WidgetWindow(200,200);
-					widgetWindow.getContainer().innerHTML = '插件需要您的腾讯微博授权, <button id="' + loginid + '">授权</button><button id="' + logoutid + '">不授权</button>';
+					// 授权确认层
+					widgetWindow = new WidgetWindow(420,210);
+					con = widgetWindow.getContainer();
+					con.innerHTML = '<p style="text-align:center;">' + manifest.name + ' 需要您的腾讯微博授权 </p><div style="width:210px;margin:0 auto;"><div style="display:block;width:85px;height:25px;background:url(./images/btns.png) no-repeat -11px -4px;cursor:hand;cursor:pointer;font-size:12px;text-align:center;line-height:25px;color:white;" href="#" id="' + loginid + '">授 权</div><div style="display:block;width:85px;height:25px;background:url(./images/btns.png) no-repeat -100px -4px;cursor:hand;cursor:pointer;font-size:12px;text-align:center;line-height:25px;margin-top:-25px;margin-left:125px;color:gray;" href="#" id="' + logoutid + '">取 消</div></div>';
 					widgetWindow.show();
 					_.find('#' + loginid)[0].onclick = function () {
+						var innerauthenabled = _.bigtable.get('innerauth','enabled');
+						if (innerauthenabled) {
+							// 不要挡住浮层授权窗
+							widgetWindow.setBottomMost();
+						}
 						_.login(function () {
 							// 移除当前授权提示窗
-							widgetWindow.remove();
+							widgetWindow._remove();
+							// 恢复原来的z轴层级
+							widgetWindow.restorezIndex();
 							// 初始化widget
 							initWidget();
 						});
+						return false;
 					};
 					_.find('#' + logoutid)[0].onclick = function () {
-						widgetWindow.remove();
+						widgetWindow._remove();
+						return false;
 					};
 					return;
 				}
-				// make sure is logged in
+				//
 				initWidget();
 			} );
 		} else {// 插件自己管理
@@ -231,10 +256,41 @@
 		}
 
 		function initWidget() {
-			var widgetWindow;
-			widgetWindow = new WidgetWindow();
-			manifest.main.call(widgetWindow);
+			var widgetWindow, undef, jqueryReady, jqueryObject;
+			widgetWindow = new WidgetWindow(320,130);
+			widgetWindow.getContainer().style.background='url(./images/loading.gif) no-repeat 50% 50%';
+			// 由组件通知已准备好绘制，隐藏loading动画
+			widgetWindow.ready = function () {
+				widgetWindow.getContainer().style.background='';
+			};
+			// 显示loading
 			widgetWindow.show();
+
+			function executeMain() {
+				// 屏蔽掉组件不应该访问的内部方法
+				manifest.main.call(widgetWindow, jqueryObject);
+				// 插件未调用show，帮它调用一下
+				if (!widgetWindow.isVisible()) widgetWindow.show();
+			}
+
+			if (manifest.css || manifest.jquery) {
+				if (manifest.jquery) {
+					jqueryReady = _.deferred.deferred();
+					_.bind('jQueryLoaded', function (jquery) {
+						jqueryObject = jquery;
+						jqueryReady.resolve();
+					});
+				}
+				_.task(
+					(manifest.css ? _.loadStyle({url: manifest.css}): 1),
+					(manifest.jquery ? _.script({url: "./js/jquery-1.8.1.js"}) : 1),
+					(jqueryReady ? jqueryReady : 1)
+				).success(executeMain).error(function (code, message) {
+					_l.error('挂件[' + manifest.name + '存在错误]，请检查manifest中css,jquery的设置，详细错误信息：' + message);
+				});
+			} else {
+				executeMain();
+			}
 		}
 
 	});
